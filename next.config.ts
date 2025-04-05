423ab29e6fae7de847693ea5bfc4c875c9059ea9
csclementino <carlossantiagoclementino36@gmail.com>
2025-04-05 15:47:23 -0300

    jj

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 const isGithubPages = process.env.NODE_ENV === 'production';
-const repo = 'teste'; // substitua pelo nome do seu repositório
+
 
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // <- ESSA LINHA AQUI
   },
   output: 'export',
-  basePath: isGithubPages ? `/${repo}` : '',
-  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 export default nextConfig;
