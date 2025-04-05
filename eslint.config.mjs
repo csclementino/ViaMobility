5d6a6f57b8b462751fade3b8adbb35d4bd4a1a44
csclementino <carlossantiagoclementino36@gmail.com>
2025-04-05 15:23:32 -0300

    h

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // <- aqui você muda para aviso
+    },
+  },
 ];
 
 export default eslintConfig;
