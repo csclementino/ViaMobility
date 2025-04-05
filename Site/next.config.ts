da6cfbe6c859a189264d85729a9895f9b692da4d
Carlos Clementino <carlossantiagoclementino36@gmail.com>
2025-04-05 16:15:06 -0300

    Update next.config.ts

--- Site/next.config.ts
+++ Site/next.config.ts
@@ -1,11 +1,13 @@
 const isGithubPages = process.env.NODE_ENV === 'production';
 
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // <- ESSA LINHA AQUI
+    ignoreDuringBuilds: true,
   },
   output: 'export',
+  basePath: isGithubPages ? '/teste' : '',
+  assetPrefix: isGithubPages ? '/teste/' : '',
 };
 
 export default nextConfig;
