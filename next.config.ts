af4278635afd17f92294c3cc6ca7d3312c4ec1c6
csclementino <carlossantiagoclementino36@gmail.com>
2025-04-05 15:28:29 -0300

    kjk

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ const isGithubPages = process.env.NODE_ENV === 'production';
 const repo = 'teste'; // substitua pelo nome do seu repositório
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // <- ESSA LINHA AQUI
+  },
   output: 'export',
   basePath: isGithubPages ? `/${repo}` : '',
   assetPrefix: isGithubPages ? `/${repo}/` : '',
